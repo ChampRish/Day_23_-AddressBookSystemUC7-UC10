6e2d4ab7bcbc17e9c5eb340001454f3ee716d0f2
RISHABH <gguptarishabh82@gmail.com>
2022-04-04 22:47:49 +0530

    Ability to ensure there is no Duplicate Entry of the same Person in a particular Address Book

---
+++ UC7_Noduplicate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_23__AddressBookSystemUC7_UC10
+{
+    public class UC7_Noduplicate
+    {
+        List<Contact> AddressList = new List<Contact>();
+        Dictionary<string, List<Contact>> MultipleAddressbook = new Dictionary<string, List<Contact>>();
+        public void AddContact(Contact newcontact)
+        {
+            AddressList.Add(newcontact);
+            MultipleAddressbook.Add(newcontact.firstname, AddressList);
+        }
+
+
+
+        public void AddUniqueContact(string uniquename)
+        {
+            foreach (var contact in AddressList)
+            {
+                if (contact.firstname.Equals(uniquename))
+                {
+                    Contact multiplecontact = new Contact();
+
+                    Console.WriteLine("Contact already exist , Enter unique name");
+                    multiplecontact.firstname = Console.ReadLine();
+                    multiplecontact.lastname = Console.ReadLine();
+                    multiplecontact.address = Console.ReadLine();
+                    multiplecontact.city = Console.ReadLine();
+                    multiplecontact.state = Console.ReadLine();
+                    multiplecontact.zip = Console.ReadLine();
+                    multiplecontact.phonenumber = Console.ReadLine();
+                    multiplecontact.emailid = Console.ReadLine();
+                    AddContact(multiplecontact);
+                }
+            }
+        }
+        public void DisplayUniqueContacts()
+        {
+            Console.WriteLine("Enter firstname to display that contact details");
+            string name = Console.ReadLine().ToLower();
+            foreach (var contacts in MultipleAddressbook)
+            {
+                if (contacts.Key == name)
+                {
+                    foreach (var data in contacts.Value)
+                    {
+                        Console.WriteLine("The Contact details of " + data.firstname + "are : \n" + data.firstname + " " + data.lastname + " " + data.address + " " + data.city + " " + data.state + " " + data.zip + " " + data.phonenumber + " " + data.emailid);
+                    }
+                }
+
+            }
+        }
+
+    }
+}
+
+
+
