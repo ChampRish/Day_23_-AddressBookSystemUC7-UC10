18450e2d8073fc36972ebe78db1a2c70e0b48f2f
RISHABH <gguptarishabh82@gmail.com>
2022-04-04 22:51:36 +0530

    Ability to search Person in a City or State across the multiple AddressBook

---
+++ uc8_SearchPerson.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_23__AddressBookSystemUC7_UC10
+{
+    internal class uc8_SearchPerson
+    {
+        List<Contact> AddressList = new List<Contact>();
+        Dictionary<string, List<Contact>> MultipleAddressbook = new Dictionary<string, List<Contact>>();
+
+        public void Search_person_city_state()
+        {
+
+            Console.WriteLine("Enter your Choice for Searching a Person in");
+            Console.WriteLine("\n1.City \n2.State");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter City Name:");
+                    String City = Console.ReadLine();
+
+                    foreach (Contact data in this.AddressList.FindAll(e => e.city == City))
+                    {
+                        Console.WriteLine(data.firstname + " " + data.lastname + " is from " + data.city);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Enter State Name:");
+                    String State = Console.ReadLine();
+
+                    foreach (Contact data in this.AddressList.FindAll(e => e.state == State))
+                    {
+                        Console.WriteLine(data.firstname + " " + data.lastname + " is from " + data.state);
+                    }
+                    break;
+
+            }
+
+
+        }
+    }
+}
